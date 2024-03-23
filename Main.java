cef7fdba1ab75d0a456f869745bee5ff2b1fe016
Marlie Denis <101306894+Marlied1@users.noreply.github.com>
2024-03-22 23:25:03 -0400

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        String message = "Hello World" + "!!";
+    
+       System.out.println(message.replace("!", "$"));
     }
 }
