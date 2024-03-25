7ca26df60d94b10c5520e37b8b65d7a880d1b19a
Marlie Denis <101306894+Marlied1@users.noreply.github.com>
2024-03-25 01:24:25 -0400

    test

--- Bible.java
+++ Bible.java
@@ -1,3 +1,39 @@
-public class Bible {
-    
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Bible extends JFrame {
+
+    private JTextField chapterField;
+    private JTextField verseField;
+    private JButton searchButton;
+    private JLabel scriptureLabel;
+
+    public Bible() {
+        setTitle("Bible");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        chapterField = new JTextField(10);
+        verseField = new JTextField(10);
+        searchButton = new JButton("Search");
+        scriptureLabel = new JLabel("Scripture goes here");
+
+        add(new JLabel("Chapter:"));
+        add(chapterField);
+        add(new JLabel("Verse:"));
+        add(verseField);
+        add(searchButton);
+        add(scriptureLabel);
+
+        setSize(300, 150);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Bible();
+    }
 }
+
